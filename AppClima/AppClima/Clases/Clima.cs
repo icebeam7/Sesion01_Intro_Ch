ecffe31c37ed78597f8cefd9444b9f344b51af61
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2017-09-16 23:52:41 +0200

    Sesion 1 - Curso Chihuahua. App de introducción

---
+++ AppClima/AppClima/Clases/Clima.cs
@@ -0,0 +1,13 @@
+﻿namespace AppClima.Clases
+{
+    public class Clima
+    {
+        public string Titulo { get; set; }
+        public string Temperatura { get; set; }
+        public string Viento { get; set; }
+        public string Humedad { get; set; }
+        public string Visibilidad { get; set; }
+        public string Amanecer { get; set; }
+        public string Ocaso { get; set; }
+    }
+}
---
+++ AppClima/AppClima/Paginas/PaginaClima.xaml.cs
@@ -0,0 +1,39 @@
+﻿using AppClima.Servicios;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppClima.Paginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaClima : ContentPage
+    {
+        public PaginaClima()
+        {
+            InitializeComponent();
+        }
+
+        private void ActualizarActivityIndicator(bool estado)
+        {
+            activityIndicator.IsRunning = estado;
+            activityIndicator.IsEnabled = estado;
+            activityIndicator.IsVisible = estado;
+        }
+
+        private async void btnBuscar_Clicked(object sender, EventArgs e)
+        {
+            ActualizarActivityIndicator(true);
+            string ciudad = txtCiudad.Text;
+
+            if (!String.IsNullOrEmpty(ciudad))
+            {
+                var clima = await ServicioClima.ConsultarClima(ciudad);
+                this.BindingContext = clima;
+            }
+            else
+                await DisplayAlert("Error", "Debes escribir una ciudad", "OK");
+
+            ActualizarActivityIndicator(false);
+        }
+    }
+}
